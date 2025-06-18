1c27d41d041e0a4239db5a7f2bc37aacc646d053
Youssef-Alshwawra <157247477+Youssef-Alshwawra@users.noreply.github.com>
2025-06-18 21:41:51 +0300

    Split Controller and module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-@Controller()
-class AppController {
-    @Get()
-    getRootRoute() {
-        return "hello there!"
-    }
+async function boostrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.listen(3000)
 }
 
+boostrap();
